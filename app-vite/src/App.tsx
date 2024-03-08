e29d6d8d15241a75d4ed4dba1336778d89bb03b3
Jamie Popkin <popkinj@littleearth.ca>
2024-03-08 12:29:50 -0800

    App.tsx is clean

--- app-vite/src/App.tsx
+++ app-vite/src/App.tsx
@@ -1,13 +1,13 @@
 import CircularProgress from "@mui/material/CircularProgress";
 import { ThemeProvider } from "@mui/material/styles";
 import { ReactKeycloakProvider } from "@react-keycloak/web";
-import AppRouter from "AppRouter";
-import { AuthStateContextProvider } from "contexts/authStateContext";
-import { ConfigContext, ConfigContextProvider } from "contexts/configContext";
+import AppRouter from "./AppRouter";
+import { AuthStateContextProvider } from "./contexts/authStateContext";
+import { ConfigContext, ConfigContextProvider } from "./contexts/configContext";
 import Keycloak from "keycloak-js";
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import appTheme from "themes/appTheme";
+import appTheme from "./themes/appTheme";
 
 const App: React.FC = () => {
   return (
