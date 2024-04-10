9c5d5ce05276668544b0b5705e71a238c016475e
Jamie Popkin <popkinj@littleearth.ca>
2024-04-10 10:45:19 -0700

    Nert 257 - Map control for layer switcher to match Leaflet control. (#35)

--- app/src/components/map/components/LayerSwitcher.tsx
+++ app/src/components/map/components/LayerSwitcher.tsx
@@ -2,8 +2,10 @@
  * Layer Switcher Component
  * Turn layers on and off
  */
-import { Box, Checkbox, FormControlLabel, FormGroup } from '@mui/material';
-import React from 'react';
+import CloseIcon from '@mui/icons-material/Close';
+import LayersIcon from '@mui/icons-material/Layers';
+import { Box, Checkbox, FormControlLabel, FormGroup, IconButton } from '@mui/material';
+import React, { useState } from 'react';
 
 export interface ILayerSwitcherProps {
   layerVisibility: {
@@ -15,12 +17,27 @@ export interface ILayerSwitcherProps {
   };
 }
 
-const style = {
+const switcherStyle = {
   position: 'absolute',
-  bottom: '90px',
-  left: '20px',
+  bottom: '120px',
+  right: '10px',
   zIndex: 1000,
   padding: '10px',
+  backgroundColor: 'white',
+  borderRadius: '5px',
+  boxShadow: '0 0 10px rgba(0,0,0,0.5)'
+};
+const switcherCloseStyle = {
+  position: 'absolute',
+  right: '2px',
+  top: '2px'
+};
+
+const buttonStyle = {
+  position: 'absolute',
+  bottom: '120px',
+  right: '10px',
+  zIndex: 1000,
   backgroundColor: 'white',
   borderRadius: '5px',
   boxShadow: '0 0 10px rgba(0,0,0,0.5)'
@@ -28,34 +45,54 @@ const style = {
 
 const LayerSwitcher = (props: ILayerSwitcherProps) => {
   const { boundary, wells, projects, wildlife, indigenous } = props.layerVisibility;
+
+  const toggleLayerswitcher = () => setSwitcherOpen(!switcherOpen);
+
+  const [switcherOpen, setSwitcherOpen] = useState(false);
+
   return (
-    <Box sx={style}>
-      Context Layers
-      <FormGroup>
-        <FormControlLabel
-          control={<Checkbox checked={boundary[0]} onClick={() => boundary[1](!boundary[0])} />}
-          label="Project Boundary"
-        />
-        <FormControlLabel
-          control={<Checkbox checked={wells[0]} onClick={() => wells[1](!wells[0])} />}
-          label="Wells"
-        />
-        <FormControlLabel
-          control={<Checkbox checked={projects[0]} onClick={() => projects[1](!projects[0])} />}
-          label="Projects & Plans"
-        />
-        <FormControlLabel
-          control={<Checkbox checked={wildlife[0]} onClick={() => wildlife[1](!wildlife[0])} />}
-          label="Wildlife"
-        />
-        <FormControlLabel
-          control={
-            <Checkbox checked={indigenous[0]} onClick={() => indigenous[1](!indigenous[0])} />
-          }
-          label="Indigenous"
-        />
-      </FormGroup>
-    </Box>
+    <div>
+      {switcherOpen ? (
+        <Box title="Open Layer Picker" sx={buttonStyle}>
+          <IconButton onClick={toggleLayerswitcher}>
+            <LayersIcon />
+          </IconButton>
+        </Box>
+      ) : (
+        <Box sx={switcherStyle}>
+          <Box title="Close Layer Picker" sx={switcherCloseStyle}>
+            <IconButton onClick={toggleLayerswitcher}>
+              <CloseIcon />
+            </IconButton>
+          </Box>
+          <b>Context Layers</b>
+          <FormGroup>
+            <FormControlLabel
+              control={<Checkbox checked={boundary[0]} onClick={() => boundary[1](!boundary[0])} />}
+              label="Project Boundary"
+            />
+            <FormControlLabel
+              control={<Checkbox checked={wells[0]} onClick={() => wells[1](!wells[0])} />}
+              label="Wells"
+            />
+            <FormControlLabel
+              control={<Checkbox checked={projects[0]} onClick={() => projects[1](!projects[0])} />}
+              label="Projects & Plans"
+            />
+            <FormControlLabel
+              control={<Checkbox checked={wildlife[0]} onClick={() => wildlife[1](!wildlife[0])} />}
+              label="Wildlife"
+            />
+            <FormControlLabel
+              control={
+                <Checkbox checked={indigenous[0]} onClick={() => indigenous[1](!indigenous[0])} />
+              }
+              label="Indigenous"
+            />
+          </FormGroup>
+        </Box>
+      )}
+    </div>
   );
 };
 
