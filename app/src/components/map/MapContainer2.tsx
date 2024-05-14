efa8feb041e68385482fdf1b7f788c0bccbb8f94
Kjartan Einarsson <kjartanreinarsson@gmail.com>
2024-05-14 15:52:50 -0700

    eslint fix

--- app/src/components/map/MapContainer2.tsx
+++ app/src/components/map/MapContainer2.tsx
@@ -169,10 +169,10 @@ let map: maplibre.Map;
 const initializeMap = (
   mapId: string,
   center: any = [-124, 57],
-  zoom: number = 6,
+  zoom = 6,
   features?: any, // There's no features when first creating a record
   layerVisibility?: any,
-  centroids: boolean = false,
+  centroids = false,
   tooltipState?: any
 ) => {
   const { boundary, wells, projects, plans, wildlife, indigenous } = layerVisibility;
