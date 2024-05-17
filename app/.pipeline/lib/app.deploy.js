7b8c9a2d5804d7444e81eb9312ed151c5c7c83f8
Oscar Inostroza <oscar.inostroza@quartech.com>
2024-05-16 19:09:29 -0600

    NERT-22a externalized maptiler api key

--- app/.pipeline/lib/app.deploy.js
+++ app/.pipeline/lib/app.deploy.js
@@ -38,7 +38,8 @@ const appDeploy = async (settings) => {
         MEMORY_REQUEST: phases[phase].memoryRequest,
         MEMORY_LIMIT: phases[phase].memoryLimit,
         REPLICAS: phases[phase].replicas,
-        REPLICAS_MAX: phases[phase].replicasMax
+        REPLICAS_MAX: phases[phase].replicasMax,
+        REACT_APP_MAPTILER_API_KEY: phases[phase].mapTiler.mapTilerSecretApiKey
       }
     })
   );
