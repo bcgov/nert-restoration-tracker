10988ca2f1a987b71cf41ca779a81b27ee0558e8
Jamie Popkin <popkinj@littleearth.ca>
2024-05-24 13:45:49 -0700

    Image thumbnails being generated

--- app/src/components/attachments/ImageUpload.tsx
+++ app/src/components/attachments/ImageUpload.tsx
@@ -37,12 +37,41 @@ const ImageUpload = () => {
 
   const handleDrop = (e: any) => {
     e.preventDefault();
+
     const files = e.dataTransfer.files;
+
+    const processImage = (image: any) => {
+      console.log('originalLength', image.length);
+      const img = new Image();
+      img.src = image;
+
+      img.onload = () => {
+        const canvas = document.createElement('canvas');
+        const ctx = canvas.getContext('2d');
+        const width = img.width;
+        const height = img.height;
+        const aspectRatio = width / height;
+
+        const res = 256; // The largest we want the thumbnail to be is 256 x 256
+        const newWidth = Math.sqrt(res * res * aspectRatio);
+        const newHeight = Math.sqrt((res * res) / aspectRatio);
+        canvas.width = newWidth;
+        canvas.height = newHeight;
+
+        ctx?.drawImage(img, 0, 0, newWidth, newHeight);
+
+        const dataUrl = canvas.toDataURL();
+
+        setImage(dataUrl);
+        console.log('newLength', dataUrl.length);
+      };
+    };
+
     if (files.length) {
       if (files[0].type === 'image/png' || files[0].type === 'image/jpeg') {
         const reader = new FileReader();
         reader.onloadend = () => {
-          setImage(reader.result);
+          processImage(reader.result);
         };
         reader.readAsDataURL(files[0]);
         setHoverText('Yum yum yum!');
