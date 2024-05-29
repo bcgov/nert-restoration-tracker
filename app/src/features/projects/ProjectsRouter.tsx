5764330747c257ed89cd91e50c486cdd26789911
Kjartan Einarsson <kjartanreinarsson@gmail.com>
2024-05-29 15:07:40 -0700

    fix merge conflict

--- app/src/features/projects/ProjectsRouter.tsx
+++ app/src/features/projects/ProjectsRouter.tsx
@@ -1,7 +1,6 @@
 import { RoleGuard, SystemRoleGuard } from 'components/security/Guards';
 import { PROJECT_ROLE, SYSTEM_ROLE } from 'constants/roles';
 import EditProjectPage from 'features/projects/edit/EditProjectPage';
-import ProjectsLayout from 'features/projects/ProjectsLayout';
 import CreateProjectPage from 'features/projects/create/CreateProjectPage';
 import ProjectsLayout from 'features/projects/ProjectsLayout';
 import ViewProjectPage from 'features/projects/view/ViewProjectPage';
