5c994f7b0526f21fb6dcdf09c615749c9552388e
Jamie Popkin <popkinj@littleearth.ca>
2024-07-04 10:43:42 -0700

    Here is the layer switcher in the plan view page

--- app/src/features/plans/view/ViewPlanPage.tsx
+++ app/src/features/plans/view/ViewPlanPage.tsx
@@ -20,6 +20,7 @@ import React, { useCallback, useContext, useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import PlanDetailsPage from './PlanDetailsPage';
 import MapContainer from 'components/map/MapContainer';
+import LayerSwitcher from 'components/map/components/LayerSwitcher';
 import { DeletePlanI18N } from 'constants/i18n';
 import { IErrorDialogProps } from 'components/dialog/ErrorDialog';
 import { Card, Chip, Tooltip } from '@mui/material';
@@ -34,6 +35,10 @@ const pageStyles = {
     '& button + button': {
       marginLeft: '1rem'
     }
+  },
+  layerSwitcherContainer: {
+    position: 'relative',
+    bottom: '-70px'
   },
   fullScreenBtn: {
     padding: '3px',
@@ -323,6 +328,9 @@ const ViewPlanPage: React.FC = () => {
                       mask={null}
                     />
                   </Box>
+                  <Box sx={pageStyles.layerSwitcherContainer}>
+                    <LayerSwitcher layerVisibility={layerVisibility} />
+                    </Box>
                 </Paper>
                 <Box mt={2} />
               </Grid>
@@ -336,24 +344,6 @@ const ViewPlanPage: React.FC = () => {
         </Card>
       </Container>
 
-      {/* <Dialog fullScreen open={openFullScreen} onClose={closeMapDialog}>
-        <Box pr={3} pl={1} display="flex" alignItems="center">
-          <Box mr={1}>
-            <IconButton onClick={closeMapDialog} aria-label="back to plan" size="large">
-              <Icon path={mdiArrowLeft} size={1} />
-            </IconButton>
-          </Box>
-        </Box>
-        <Box display="flex" height="100%" flexDirection="column">
-          <Box flex="1 1 auto">
-            <MapContainer
-              mapId={'plan_location_map'}
-              layerVisibility={layerVisibility}
-              features={planWithDetails.location.geometry}
-            />
-          </Box>
-        </Box>
-      </Dialog> */}
     </>
   );
 };
