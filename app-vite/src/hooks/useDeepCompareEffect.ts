e19c2d573524dba4b465c0b7cbe5fce2eb8ea547
Kjartan Einarsson <kjartanreinarsson@gmail.com>
2024-07-04 11:38:40 -0700

    remove app vite

--- app-vite/src/hooks/useDeepCompareEffect.ts
+++
@@ -1,9 +0,0 @@
-import { DependencyList, EffectCallback, useEffect } from 'react';
-import { useDeepCompareMemo } from './useDeepCompareMemo';
-
-export function useDeepCompareEffect(
-  callback: EffectCallback,
-  dependencies: DependencyList
-): ReturnType<typeof useEffect> {
-  return useEffect(callback, useDeepCompareMemo(dependencies));
-}
