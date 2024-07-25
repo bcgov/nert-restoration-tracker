f1572904dd525775e0157b3a9699284b49363497
oscar-ip <136661790+oscar-ip@users.noreply.github.com>
2024-07-25 15:33:22 -0600

    NERT-535 Non Auth Location view added title and export button (#137)
    Co-authored-by: Oscar Inostroza <oscar.inostroza@quartech.com>

--- app/src/features/projects/list/ProjectsListPage.tsx
+++ app/src/features/projects/list/ProjectsListPage.tsx
@@ -217,14 +217,14 @@ const ProjectsListPage: React.FC<IProjectsListProps> = (props) => {
         )}
         {numSelected > 0 ? (
           <Button
-            sx={{ height: '3rem', width: '11rem' }}
+            sx={{ height: '2.8rem', width: '11rem', fontWeight: 700 }}
             color="primary"
             variant="outlined"
             disableElevation
             data-testid="export-project-button"
             aria-label={ProjectTableI18N.exportProjectsData}
             startIcon={<Icon path={mdiExport} size={1} />}>
-            <strong>{TableI18N.exportData}</strong>
+            {TableI18N.exportData}
           </Button>
         ) : (
           <InfoDialog isProject={true} infoContent={'paged table'} />
