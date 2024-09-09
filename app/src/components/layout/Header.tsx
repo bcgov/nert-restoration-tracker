b34bcd79ddf7c2634dfe900368f4b860ff1f973c
Oscar Inostroza <oscar.inostroza@quartech.com>
2024-09-09 17:39:20 -0600

    NERT-639 fixed versioning and removed conole logs

--- app/src/components/layout/Header.tsx
+++ app/src/components/layout/Header.tsx
@@ -125,10 +125,10 @@ const Header: React.FC = () => {
 
   const email = codes.data?.branding.find((data) => data.name == 'email')?.value || '';
 
-  const mmm = config?.VERSION ? config.VERSION.split('-')[1] : '0.0.0';
-  console.log('***Config', config);
+  const mmm = config?.VERSION
+    ? config.VERSION.split('-')[1] + '.' + config?.CHANGE_VERSION
+    : '0.0.0';
   const nert_version = mmm ?? '0.0.0.NA';
-  console.log('***nert_version', nert_version);
   const nert_environment = config?.REACT_APP_NODE_ENV || 'undefined';
 
   const authStateContext = useAuthStateContext();
